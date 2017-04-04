678793f22f2876387ce2454c46d57551abc10951
mldn <807658435@qq.com>
2017-04-04 18:09:01 +0800

    gai le

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 ﻿public class Hello{
 	public static void public static void main(String[] args) {
 		System.out.println("傻逼");
-		System.out.println("傻逼");
+		
 <<<<<<< HEAD
 	System.out.println("二黄");	
 	
