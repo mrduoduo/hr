df749e229c60c5037c2a98abb3f2ccf22ec5ca19
mldn <807658435@qq.com>
2017-04-04 18:42:58 +0800

    add nihao.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 ﻿public class Hello{
 	public static void public static void main(String[] args) {
-		System.out.println("傻逼");
+		    System.out.println("傻逼");
 			System.out.println("傻逼是你");
+            System.out.println("傻逼是你");
 <<<<<<< HEAD
 	System.out.println("二黄");	
 	
