00375deba8597f05a26c65df6ae1811e4b56ea96
mldn <807658435@qq.com>
2017-04-04 17:19:39 +0800

    feng zhi ti jiao

--- Hello.java
+++ Hello.java
@@ -2,8 +2,7 @@
 	public static void public static void main(String[] args) {
 		System.out.println("傻逼");
 		System.out.println("傻逼");
-		System.out.println("二黄");
-	
+		
 <<<<<<< HEAD
                
 =======
