30fea70d3da8cd3ca9cbed19efd9b57d822e886c
mldn <807658435@qq.com>
2017-04-04 16:46:39 +0800

    master java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
-public class Hello{
+﻿public class Hello{
 	public static void public static void main(String[] args) {
 		System.out.println("傻逼");
 		System.out.println("傻逼");
 		System.out.println("二黄");
 		System.out.println("傻逼");
+               
 	}
 }
