ed8e93337b900e985fe94da7a44d124ffb42c80d
mldn <807658435@qq.com>
2017-04-04 11:39:32 +0800

    add two lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void public static void main(String[] args) {
-		System.out.println("Hello,Git");
+		
+		//System.out.println("傻逼");
+		System.out.println("二黄");
 	}
 }
