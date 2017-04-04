e61332b29ee8ff862b21c4d64c46a9381ca3e856
mldn <807658435@qq.com>
2017-04-04 17:18:40 +0800

    di er ci hebing

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@
 	public static void public static void main(String[] args) {
 		System.out.println("傻逼");
 		System.out.println("傻逼");
-		System.out.println("二黄");
+	System.out.println("二黄");	
+	
+
 	
 <<<<<<< HEAD
                
